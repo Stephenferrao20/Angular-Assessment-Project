e7e186a90f1d5e45a2d00cb24b18f51cc4221dcc
Stephenferrao20 <stephenferrao20.in@gmail.com>
2026-02-24 13:25:47 +0530

    feat:setup the ci-cd pielines and aws ec2 config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,6 +9,12 @@ app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: "*",   
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 const db = require("./app/models");
 db.mongoose
